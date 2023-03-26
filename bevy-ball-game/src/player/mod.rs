c33deb933823a9c4df24143801a50101ecef16ea
Luiz F. A. de Prá <rawarkanis@gmail.com>
2023-03-26 19:08:20 -0300

    Add ep08 code

--- bevy-ball-game/src/player/mod.rs
+++ bevy-ball-game/src/player/mod.rs
@@ -5,13 +5,20 @@ use bevy::prelude::*;
 
 use self::systems::*;
 
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub enum PlayerSystemSet {
+    Movement,
+    Confinement,
+}
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player)
-            .add_system(player_movement)
-            .add_system(confine_player_movement)
+        app.configure_set(PlayerSystemSet::Movement.before(PlayerSystemSet::Confinement))
+            .add_startup_system(spawn_player)
+            .add_system(player_movement.in_set(PlayerSystemSet::Movement))
+            .add_system(confine_player_movement.in_set(PlayerSystemSet::Confinement))
             .add_system(enemy_hit_player)
             .add_system(player_hit_star);
     }
