f185634055c260621c7802873bc0f25c487bef54
Luiz F. A. de Prá <rawarkanis@gmail.com>
2023-03-30 14:07:30 -0300

    Add first parte of ep10 code

--- bevy-ball-game/src/game/mod.rs
+++ bevy-ball-game/src/game/mod.rs
@@ -33,7 +33,7 @@ impl Plugin for GamePlugin {
 
 #[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
 pub enum SimulationState {
-    Running,
     #[default]
+    Running,
     Paused,
 }
